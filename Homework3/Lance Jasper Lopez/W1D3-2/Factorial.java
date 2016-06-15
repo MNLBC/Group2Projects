71df048fd340f5717579cf5af9470251aa67948e
Ziegfreid Morissey Flameno <flamezi2@oocl.com>
2016-06-15 19:09:22 +0800

    Changed Folder Names

---
+++ Homework3/Lance Jasper Lopez/W1D3-2/Factorial.java
@@ -0,0 +1,15 @@
+
+public class Factorial {
+	public static void main(String[] args){
+	
+	}
+	
+	public static int Factorial(int n){
+		int result = 1;
+		for (int i = 1; i <= n; i++) {
+			   result = result * i;
+			}
+		
+		return result;
+	}
+}
