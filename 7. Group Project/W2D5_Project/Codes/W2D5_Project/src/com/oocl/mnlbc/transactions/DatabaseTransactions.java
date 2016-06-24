ddd341f5370b6063d65107c79e3ebd2530111cb9
Kassandra Fuentes <fuentka@oocl.com>
2016-06-24 16:21:59 +0800

    Login, Register, Default page

--- 7. Group Project/W2D5_Project/Codes/W2D5_Project/src/com/oocl/mnlbc/transactions/DatabaseTransactions.java
+++ 7. Group Project/W2D5_Project/Codes/W2D5_Project/src/com/oocl/mnlbc/transactions/DatabaseTransactions.java
@@ -15,7 +15,7 @@ public class DatabaseTransactions {
       String driver = "oracle.jdbc.driver.OracleDriver";
       String url = "jdbc:oracle:thin:@localhost:1521:xe";
       String username = "system";
-      String password = "august22"; //change password of your database system
+      String password = "admin"; //change password of your database system
       Connection conn = null;
       try {
           Class.forName(driver); //classLoader,加载对应驱动
