fe3ed237af9e0fc6412d8662dab8e9aff6fbb299
Kassandra Fuentes <fuentka@oocl.com>
2016-06-24 15:53:41 +0800

    Default page

---
+++ 7. Group Project/W2D5_Project/Codes/W2D5_Project/src/com/oocl/mnlbc/DefaultPage.java
@@ -0,0 +1,77 @@
+package com.oocl.mnlbc;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.oocl.mnlbc.models.Client;
+
+public class DefaultPage {
+
+   public static Client client;
+
+   @SuppressWarnings("resource")
+   public static void main(String[] args) {
+
+      Scanner scanner = new Scanner(System.in);
+      boolean validChoice = false;
+      client = new Client();
+      RegisterClient form1 = new RegisterClient();
+      LoginClient form2 = new LoginClient();
+
+      System.out.println("---------------------------------------------------------");
+      System.out.println("----------------------W E L C O M E----------------------");
+      System.out.println("---------------------------------------------------------");
+
+      while (!validChoice) {
+         System.out.print("\nChoose:\n [1] Register [2] Log-In  [3] Log-In anonymously : ");
+         String choice = scanner.next();
+
+         if (choice.equals("1")) {
+            form1.signUp(client);
+            validChoice = true;
+         } else if (choice.equals("2")) {
+            form2.login(client);
+            validChoice = true;
+         } else if (choice.equals("3")) {
+//           enter chat
+            validChoice = true;
+         } else {
+            System.out.println("Please select the right choice");
+         }
+      }
+   }
+
+   @SuppressWarnings("resource")
+   public static Client displayDefaultPage() {
+
+      Scanner scanner = new Scanner(System.in);
+      boolean validChoice = false;
+      client = new Client();
+      RegisterClient form1 = new RegisterClient();
+      LoginClient form2 = new LoginClient();
+
+      System.out.println("---------------------------------------------------------");
+      System.out.println("----------------------W E L C O M E----------------------");
+      System.out.println("---------------------------------------------------------");
+
+      while (!validChoice) {
+         System.out.print("\nChoose:\n [1] Register [2] Log-In  [3] Log-In anonymously : ");
+         String choice = scanner.next();
+
+         if (choice.equals("1")) {
+            form1.signUp(client);
+            validChoice = true;
+         } else if (choice.equals("2")) {
+            form2.login(client);
+            validChoice = true;
+         } else if (choice.equals("3")) {
+//           enter chat
+            validChoice = true;
+         } else {
+            System.out.println("Please select the right choice");
+         }
+      }
+      return client;
+   }
+}
