04d2e5fff2033446871075725f5ad28a69f0acf0
DannaSoquiat <danna.pauline.soquiat@oocl.com>
2016-06-24 17:48:18 +0800

    Session Class edited

--- 7. Group Project/W2D5_Project/Codes/W2D5_Project/src/com/oocl/mnlbc/models/Session.java
+++ 7. Group Project/W2D5_Project/Codes/W2D5_Project/src/com/oocl/mnlbc/models/Session.java
@@ -1,29 +1,22 @@
 package com.oocl.mnlbc.models;
 
-import java.util.List;
-
 /**
  * Base Session Class
- * @author FLAMEZI2 Group 2
+ * 
+ * @author Danna  Group 2
  *
  */
 public class Session {
 
    private Long sessionId;
-   private List<Client> clientList;
-   private String timestamp;
+   private String start;
+   private String end;
 
-   /**
-    * new Session Constructor
-    * @param sessionId
-    * @param clientList
-    * @param timestamp
-    */
-   public Session(Long sessionId, List<Client> clientList, String timestamp) {
+   public Session(Long sessionId, String start, String end) {
       super();
       this.sessionId = sessionId;
-      this.clientList = clientList;
-      this.timestamp = timestamp;
+      this.start = start;
+      this.end = end;
    }
 
    public Long getSessionId() {
@@ -34,20 +27,28 @@ public class Session {
       this.sessionId = sessionId;
    }
 
-   public List<Client> getClientList() {
-      return clientList;
+   public String getStart() {
+      return start;
    }
 
-   public void setClientList(List<Client> clientList) {
-      this.clientList = clientList;
+   public void setStart(String start) {
+      this.start = start;
    }
 
-   public String getTimestamp() {
-      return timestamp;
+   public String getEnd() {
+      return end;
    }
 
-   public void setTimestamp(String timestamp) {
-      this.timestamp = timestamp;
+   public void setEnd(String end) {
+      this.end = end;
    }
+
+   /**
+    * new Session Constructor
+    * 
+    * @param sessionId
+    * @param clientList
+    * @param timestamp
+    */
 
 }
--- 7. Group Project/W2D5_Project/Codes/W2D5_Project/src/com/oocl/mnlbc/transactions/ChatServer.java
+++ 7. Group Project/W2D5_Project/Codes/W2D5_Project/src/com/oocl/mnlbc/transactions/ChatServer.java
@@ -1,5 +1,75 @@
 package com.oocl.mnlbc.transactions;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.oocl.mnlbc.models.Client;
+import com.oocl.mnlbc.models.Session;
+import com.oocl.mnlbc.utils.Timestamp;
+
+/**
+ * Server class
+ * 
+ * @author Danna
+ *
+ */
 public class ChatServer {
+
+	ServerSocket serverSocket;
+	List<Socket> socketList = new ArrayList<Socket>();
+	List<Client> clientList = new ArrayList<Client>();
+	private Client client;	
+	private int count = 0;
+	Socket socket = null;
+	Timestamp timestamp;
+	Session session;
+
+	/**
+	 * method to start server
+	 * 
+	 * @throws IOException
+	 */
+	@SuppressWarnings("resource")
+	public void startWork() throws IOException {
+		serverSocket = new ServerSocket(7777);
+		while (true) {
+			socket = serverSocket.accept();
+			long sessid = DatabaseTransactions.getActiveSessionID();
+			count++;
+			clientList.add(client);
+			if (count > 1) {
+				if (sessid == 0) {
+					session = new Session(0L, clientList, timestamp.getTimestamp());
+				//	DatabaseTransactions.createSession(session);
+				} //else
+				//	DatabaseTransactions.updateActiveSession(clientList);
+			}
+			// Session s = new Session
+			// clients.setCount(clients.getCount() + 1);
+			System.out.println(count + " client" + (count > 1 ? "s" : "") + " connected to the server.");
+			socketList.add(socket);
+			new Chat(count, socket, socketList, client, session).run();
+			
+			
+		}
+	}
+
+	/**
+	 * Main method that creates server instance and starts it
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
+	public void ChatServer(Client client) throws IOException {
+		this.client = client;
+
+	}
+
+	public void StartServer() throws IOException {
+		this.startWork();
+	}
 
 }
