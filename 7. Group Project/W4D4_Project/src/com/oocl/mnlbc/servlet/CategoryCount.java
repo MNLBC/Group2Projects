90b1c2fb13fcc6c7b3e7d95cb308c6423cd65fc6
William "The Dragon" Kalingasan <william.kalingasan@oocl.com>
2016-07-10 19:45:38 +0800

    Update

---
+++ 7. Group Project/W4D4_Project/src/com/oocl/mnlbc/servlet/CategoryCount.java
@@ -0,0 +1,43 @@
+package com.oocl.mnlbc.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.oocl.mnlbc.dao.ProductDAOImpl;
+
+/**
+ * Servlet implementation class CategoryCount
+ */
+@WebServlet("/CategoryCount")
+public class CategoryCount extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CategoryCount() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ProductDAOImpl prodDao = new ProductDAOImpl();
+		response.getWriter().append(prodDao.countProductByCat());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
