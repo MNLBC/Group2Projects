4c70da89ba483ab7c0fbb72d87780723a69587dd
Meynard Denoyo <meynard.denoyo@oocl.com>
2016-07-15 10:17:25 +0800

    imported servlets

---
+++ 7. Group Project/W5D5_Project/src/com/oocl/mnlbc/servlet/ProductServlet.java
@@ -0,0 +1,60 @@
+package com.oocl.mnlbc.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.oocl.mnlbc.bean.Product;
+import com.oocl.mnlbc.dao.ProductDAO;
+import com.oocl.mnlbc.dao.ProductDAOImpl;
+import com.oocl.mnlbc.util.JsonParser;
+
+/**
+ * Servlet implementation class ProductServlet
+ */
+@WebServlet("/getProducts")
+public class ProductServlet extends HttpServlet {
+
+   private static final long serialVersionUID = 1L;
+
+   /**
+    * @see HttpServlet#HttpServlet()
+    */
+   public ProductServlet() {
+      super();
+   }
+
+   /**
+    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+    */
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      ProductDAOImpl prodDao = new ProductDAOImpl();
+      String category = request.getParameter("category");
+      String msg = "";
+      List<Product> prodList = new ArrayList<Product>();
+      if (category == null) {
+         prodList = prodDao.getProducts();
+      } else {
+         prodList = prodDao.getProductsByCategory(category);
+      }
+      msg = JsonParser.toProductJson(prodList);
+      response.getWriter().append(msg);
+      // response.getWriter().append(prodDao.countProductByCat());
+   }
+
+   /**
+    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+    */
+   protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+      doGet(request, response);
+   }
+
+}
