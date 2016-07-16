91f109e148a446dd4e2b5a9ff7c0140d1495a376
DannaSoquiat <danna.pauline.soquiat@oocl.com>
2016-07-16 17:41:28 +0800

    User and Product DAO and DAOImpl

--- 7. Group Project/W5D5_Project/src/com/oocl/mnlbc/dao/ProductDAO.java
+++ 7. Group Project/W5D5_Project/src/com/oocl/mnlbc/dao/ProductDAO.java
@@ -5,16 +5,15 @@ import java.util.List;
 import com.oocl.mnlbc.model.Product;
 
 public interface ProductDAO {
-	
+
 	public void addProduct(Product p);
+
 	public void updateProduct(Product p);
+
 	public List<Product> listProduct();
+
 	public Product getProductByCategory(int id);
+
 	public void removeProduct(int id);
-	
-	
-//	public List<Product> getProducts();
-//	public List <Product> getProductsByCategory(String category);
-//	public int updateStock(String[] array);
-//	public Product getProduct(String id);
+
 }
