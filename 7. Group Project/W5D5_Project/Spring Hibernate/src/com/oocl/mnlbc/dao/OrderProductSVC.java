eccaddf4f9e7d13a0618b43414be5dbe8489a992
Kassandra Fuentes <fuentka@oocl.com>
2016-07-18 12:48:13 +0800

    Apply right files :)

---
+++ 7. Group Project/W5D5_Project/Spring Hibernate/src/com/oocl/mnlbc/dao/OrderProductSVC.java
@@ -0,0 +1,15 @@
+package com.oocl.mnlbc.dao;
+
+import java.util.List;
+
+import com.oocl.mnlbc.model.CartProduct;
+import com.oocl.mnlbc.model.Order;
+import com.oocl.mnlbc.model.Product;
+
+public interface OrderProductSVC {
+	public int addProduct(Order order,Product prod,int qty);
+//	public int removeProduct(String orderId, String productId, int qty);
+	public List<Product> getRelatedProducts(Order order);
+	public List<CartProduct> getCartProducts(String orderId);
+	public int updateProduct(String orderId, String productId, int qty);
+}
---
+++ 7. Group Project/W5D5_Project/Spring Hibernate/src/com/oocl/mnlbc/dao/ProductDAO.java
@@ -0,0 +1,19 @@
+package com.oocl.mnlbc.dao;
+
+import java.util.List;
+
+import com.oocl.mnlbc.model.Product;
+
+public interface ProductDAO {
+
+	public int addProduct(Product p);
+
+	public int updateProduct(Product p);
+
+	public List<Product> listProduct();
+
+	public List<Product> getProductByCategory(String cat);
+
+	public int removeProduct(int id);
+
+}
