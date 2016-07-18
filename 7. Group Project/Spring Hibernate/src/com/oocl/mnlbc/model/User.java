024b3aed9ad95575aaa0398d9d591ed5784509a8
Lance Jasper Lopez <lancejasperlopez@gmail.com>
2016-07-18 11:09:55 +0800

    Added sequence in user model

--- 7. Group Project/Spring Hibernate/src/com/oocl/mnlbc/model/User.java
+++ 7. Group Project/Spring Hibernate/src/com/oocl/mnlbc/model/User.java
@@ -5,7 +5,10 @@ package com.oocl.mnlbc.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 /**
@@ -13,183 +16,185 @@ import javax.persistence.Table;
  *
  */
 @Entity
-@Table(name="USER")
+@Table(name = "USER")
 public class User {
 
 	@Id
-	@Column(name="userId")
-   private long userId;
-   private String userFname;
-   private String userLname;
-   private String userEmail;
-   private String userAddress1;
-   private String userAddress2;
-   private String userSp;
-   public String getUserAddress1() {
-	return userAddress1;
-}
+	@Column(name = "userId")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_Sequence")
+	@SequenceGenerator(name = "id_Sequence", sequenceName = "USERS_SEQ2")
+	private long userId;
+	private String userFname;
+	private String userLname;
+	private String userEmail;
+	private String userAddress1;
+	private String userAddress2;
+	private String userSp;
 
-public void setUserAddress1(String userAddress1) {
-	this.userAddress1 = userAddress1;
-}
+	public String getUserAddress1() {
+		return userAddress1;
+	}
 
-public String getUserAddress2() {
-	return userAddress2;
-}
+	public void setUserAddress1(String userAddress1) {
+		this.userAddress1 = userAddress1;
+	}
 
-public void setUserAddress2(String userAddress2) {
-	this.userAddress2 = userAddress2;
-}
+	public String getUserAddress2() {
+		return userAddress2;
+	}
 
-public String getUserSp() {
-	return userSp;
-}
+	public void setUserAddress2(String userAddress2) {
+		this.userAddress2 = userAddress2;
+	}
 
-public void setUserSp(String userSp) {
-	this.userSp = userSp;
-}
+	public String getUserSp() {
+		return userSp;
+	}
 
-private String userCity;
-   private String userCountry;
-   private String userPass;
-   private String userType;
+	public void setUserSp(String userSp) {
+		this.userSp = userSp;
+	}
 
-   public User(long userId, String userFname, String userLname, String userEmail, String userStreet, String userCity,
-		String userCountry, String userPass, String userType) {
-	super();
-	this.userId = userId;
-	this.userFname = userFname;
-	this.userLname = userLname;
-	this.userEmail = userEmail;
-	this.userCity = userCity;
-	this.userCountry = userCountry;
-	this.userPass = userPass;
-	this.userType = userType;
-}
-   
-   public User(){
-	   
-   }
+	private String userCity;
+	private String userCountry;
+	private String userPass;
+	private String userType;
 
-/**
-    * @return the userId
-    */
-   public long getUserId() {
-      return userId;
-   }
+	public User(long userId, String userFname, String userLname, String userEmail, String userStreet, String userCity,
+			String userCountry, String userPass, String userType) {
+		super();
+		this.userId = userId;
+		this.userFname = userFname;
+		this.userLname = userLname;
+		this.userEmail = userEmail;
+		this.userCity = userCity;
+		this.userCountry = userCountry;
+		this.userPass = userPass;
+		this.userType = userType;
+	}
 
-   /**
-    * @param userId
-    *           the userId to set
-    */
-   public void setUserId(long userId) {
-      this.userId = userId;
-   }
+	public User() {
 
-   /**
-    * @return the userFname
-    */
-   public String getUserFname() {
-      return userFname;
-   }
+	}
 
-   /**
-    * @param userFname
-    *           the userFname to set
-    */
-   public void setUserFname(String userFname) {
-      this.userFname = userFname;
-   }
+	/**
+	 * @return the userId
+	 */
+	public long getUserId() {
+		return userId;
+	}
 
-   /**
-    * @return the userLname
-    */
-   public String getUserLname() {
-      return userLname;
-   }
+	/**
+	 * @param userId
+	 *            the userId to set
+	 */
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
 
-   /**
-    * @param userLname
-    *           the userLname to set
-    */
-   public void setUserLname(String userLname) {
-      this.userLname = userLname;
-   }
+	/**
+	 * @return the userFname
+	 */
+	public String getUserFname() {
+		return userFname;
+	}
 
-   /**
-    * @return the userEmail
-    */
-   public String getUserEmail() {
-      return userEmail;
-   }
+	/**
+	 * @param userFname
+	 *            the userFname to set
+	 */
+	public void setUserFname(String userFname) {
+		this.userFname = userFname;
+	}
 
-   /**
-    * @param userEmail
-    *           the userEmail to set
-    */
-   public void setUserEmail(String userEmail) {
-      this.userEmail = userEmail;
-   }
+	/**
+	 * @return the userLname
+	 */
+	public String getUserLname() {
+		return userLname;
+	}
 
+	/**
+	 * @param userLname
+	 *            the userLname to set
+	 */
+	public void setUserLname(String userLname) {
+		this.userLname = userLname;
+	}
 
-   /**
-    * @return the userCity
-    */
-   public String getUserCity() {
-      return userCity;
-   }
+	/**
+	 * @return the userEmail
+	 */
+	public String getUserEmail() {
+		return userEmail;
+	}
 
-   /**
-    * @param userCity
-    *           the userCity to set
-    */
-   public void setUserCity(String userCity) {
-      this.userCity = userCity;
-   }
+	/**
+	 * @param userEmail
+	 *            the userEmail to set
+	 */
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
 
-   /**
-    * @return the userCountry
-    */
-   public String getUserCountry() {
-      return userCountry;
-   }
+	/**
+	 * @return the userCity
+	 */
+	public String getUserCity() {
+		return userCity;
+	}
 
-   /**
-    * @param userCountry
-    *           the userCountry to set
-    */
-   public void setUserCountry(String userCountry) {
-      this.userCountry = userCountry;
-   }
+	/**
+	 * @param userCity
+	 *            the userCity to set
+	 */
+	public void setUserCity(String userCity) {
+		this.userCity = userCity;
+	}
 
-   /**
-    * @return the userPass
-    */
-   public String getUserPass() {
-      return userPass;
-   }
+	/**
+	 * @return the userCountry
+	 */
+	public String getUserCountry() {
+		return userCountry;
+	}
 
-   /**
-    * @param userPass
-    *           the userPass to set
-    */
-   public void setUserPass(String userPass) {
-      this.userPass = userPass;
-   }
+	/**
+	 * @param userCountry
+	 *            the userCountry to set
+	 */
+	public void setUserCountry(String userCountry) {
+		this.userCountry = userCountry;
+	}
 
-   /**
-    * @return the userType
-    */
-   public String getUserType() {
-      return userType;
-   }
+	/**
+	 * @return the userPass
+	 */
+	public String getUserPass() {
+		return userPass;
+	}
 
-   /**
-    * @param userType
-    *           the userType to set
-    */
-   public void setUserType(String userType) {
-      this.userType = userType;
-   }
+	/**
+	 * @param userPass
+	 *            the userPass to set
+	 */
+	public void setUserPass(String userPass) {
+		this.userPass = userPass;
+	}
+
+	/**
+	 * @return the userType
+	 */
+	public String getUserType() {
+		return userType;
+	}
+
+	/**
+	 * @param userType
+	 *            the userType to set
+	 */
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
 
 }
