58068ca5137d5e2a6547d75e0494a7b1ad1e9d65
Ziegfreid Morissey Flameno <flamezi2@oocl.com>
2016-07-22 15:10:55 +0800

    remove unused pictures + JmsAdminController update

--- 7. Group Project/W6D5_Project/W6D5_Project/Spring Hibernate/src/com/oocl/mnlbc/controller/JmsAdminController.java
+++ 7. Group Project/W6D5_Project/W6D5_Project/Spring Hibernate/src/com/oocl/mnlbc/controller/JmsAdminController.java
@@ -3,6 +3,11 @@
  */
 package com.oocl.mnlbc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +38,8 @@ public class JmsAdminController {
    }
 
    @RequestMapping("/sendAdminMessage")
-   public int sendAdminMessage(@RequestParam("username") String username, @RequestParam("message") String message) {
+   public int sendAdminMessage(@RequestParam("username") String username, @RequestParam("message") String message,
+      HttpServletRequest request) {
       try {
          JmsProducer.sendRequest("adminTopic", username, message);
          return 1;
@@ -41,5 +47,27 @@ public class JmsAdminController {
          LogUtil.logMsg(LogType.ERROR, "Error: " + e);
          return 0;
       }
+   }
+
+   @SuppressWarnings("unchecked")
+   @RequestMapping("/getAllMessages")
+   public List<String> getAllMessages(HttpServletRequest request) {
+      List<String> requestList = (List<String>) request.getServletContext().getAttribute("requestList");
+      LogUtil.logMsg(LogType.INFO, "Number of Requests: " + requestList.size());
+      return requestList;
+   }
+
+   @SuppressWarnings("unchecked")
+   @RequestMapping("/addMessage")
+   public Boolean addMessage(@RequestParam("message") String message, HttpServletRequest request) {
+      List<String> requestList = (List<String>) request.getServletContext().getAttribute("requestList");
+      if (requestList == null) {
+         requestList = new ArrayList<String>();
+      }
+      requestList.add(message);
+      request.getServletContext().setAttribute("requestList", requestList);
+      LogUtil.logMsg(LogType.INFO, "Requesting User: " + message);
+      LogUtil.logMsg(LogType.INFO, "Number of Requests: " + requestList.size());
+      return true;
    }
 }
