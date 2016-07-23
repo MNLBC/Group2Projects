9b0cf22a889c9d2dc7c823e0193240d96e9c0c24
Meynard Denoyo <meynard.denoyo@oocl.com>
2016-07-23 22:59:31 +0800

    edited JMSListener

--- 7. Group Project/W6D5_Project/W6D5_Project/Spring Hibernate/src/com/oocl/mnlbc/jms/listener/JmsListener.java
+++ 7. Group Project/W6D5_Project/W6D5_Project/Spring Hibernate/src/com/oocl/mnlbc/jms/listener/JmsListener.java
@@ -5,8 +5,15 @@ import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.oocl.mnlbc.model.Request;
+import com.oocl.mnlbc.svc.inf.OrderProductSVC;
+import com.oocl.mnlbc.svc.inf.RequestSVC;
 import com.oocl.mnlbc.util.LogType;
 import com.oocl.mnlbc.util.LogUtil;
+import com.oocl.mnlbc.util.Timestamp;
 
 /**
  * JMS Listener
@@ -16,12 +23,21 @@ import com.oocl.mnlbc.util.LogUtil;
  */
 public class JmsListener implements MessageListener {
 
+   private RequestSVC requestSVC;
+
+   @Autowired(required = true)
+   @Qualifier(value = "requestService")
+   public void setOrderProdService(RequestSVC requestSVC) {
+      this.requestSVC = requestSVC;
+   }
+   
    public void onMessage(Message message) {
       if (message != null && message instanceof TextMessage) {
          try {
             TextMessage textMessage = (TextMessage) message;
             LogUtil.logMsg(LogType.INFO, textMessage.getText());
             LogUtil.logMsg(LogType.INFO, message.getJMSDestination().toString());
+            this.requestSVC.createRequest(textMessage.getText());
          } catch (JMSException e) {
             e.printStackTrace();
          }
