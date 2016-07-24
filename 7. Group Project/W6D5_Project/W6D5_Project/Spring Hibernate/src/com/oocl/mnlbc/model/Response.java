c06923f58ad657f81509b2277019fe1044477114
Kassandra Fuentes <fuentka@oocl.com>
2016-07-24 15:49:05 +0800

    Edit response model

--- 7. Group Project/W6D5_Project/W6D5_Project/Spring Hibernate/src/com/oocl/mnlbc/model/Response.java
+++ 7. Group Project/W6D5_Project/W6D5_Project/Spring Hibernate/src/com/oocl/mnlbc/model/Response.java
@@ -36,12 +36,6 @@ public class Response {
 	         case 2 :
 	        	 LogUtil.logMsg(LogType.WARN, "User is null"); 
 		            break;
-	         case 3 :
-	        	 LogUtil.logMsg(LogType.WARN, "Login Failed"); 
-		            break;
-	         case 4 :
-	        	 LogUtil.logMsg(LogType.INFO, "Login Successful"); 
-		            break;
 	         default :
 	        	 LogUtil.logMsg(LogType.WARN, "Binding error");
 	      }
