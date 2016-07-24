0c4a61e256f1e92269f65f5e3ad59ea6c87c74a9
William Kalingasan <william.kalingasan@oocl.com>
2016-07-24 12:09:20 +0800

    TempRequestDAO created for createRequest

--- 7. Group Project/W6D5_Project/W6D5_Project/Spring Hibernate/src/com/oocl/mnlbc/jms/listener/JmsListener.java
+++ 7. Group Project/W6D5_Project/W6D5_Project/Spring Hibernate/src/com/oocl/mnlbc/jms/listener/JmsListener.java
@@ -8,6 +8,7 @@ import javax.jms.TextMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import com.oocl.mnlbc.dao.impl.TempRequestDAOImpl;
 import com.oocl.mnlbc.model.Request;
 import com.oocl.mnlbc.svc.inf.OrderProductSVC;
 import com.oocl.mnlbc.svc.inf.RequestSVC;
@@ -34,10 +35,11 @@ public class JmsListener implements MessageListener {
    public void onMessage(Message message) {
       if (message != null && message instanceof TextMessage) {
          try {
+        	TempRequestDAOImpl tempRequestDAO = new TempRequestDAOImpl();
             TextMessage textMessage = (TextMessage) message;
             LogUtil.logMsg(LogType.INFO, textMessage.getText());
             LogUtil.logMsg(LogType.INFO, message.getJMSDestination().toString());
-            this.requestSVC.createRequest(textMessage.getText());
+            tempRequestDAO.createRequest(textMessage.getText());
          } catch (JMSException e) {
             e.printStackTrace();
          }
